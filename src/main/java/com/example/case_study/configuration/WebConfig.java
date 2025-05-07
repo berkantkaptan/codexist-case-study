1be812733cad0f49b359841a5c70066c4129c0d4
berkantkaptan <berkant.kaptan@hotmail.com>
2025-05-07 20:14:54 +0300

    Webconfig fe url address update

--- src/main/java/com/example/case_study/configuration/WebConfig.java
+++ src/main/java/com/example/case_study/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://codexist-case-study-fe.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
